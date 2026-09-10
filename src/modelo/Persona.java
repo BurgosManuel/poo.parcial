3746cb96826edb8379fe29b96c80f3d5f93a13b4
BurgosManuel <trapecioinv@gmail.com>
2026-09-10 08:34:13 -0300

    Persona

---
+++ src/modelo/Persona.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author mburgos
+ */
+public class Persona {
+    private String dni;
+    private String nombre;
+    private int edad;
+    private char sexo; // M o F
+    
+    public boolean esMayorEdad() {
+        return edad >= 18;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+}
