8dc511d791bad99ce974759bc4b30651f2ffc8fb
BurgosManuel <trapecioinv@gmail.com>
2026-09-10 09:09:58 -0300

    Interface reporte

---
+++ src/interfaces/IVistaReporte.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mburgos
+ */
+public interface IVistaReporte {
+    public void cargarDatos(ArrayList items);
+    public void setPacientesMayores(int cantidad);
+    public void setPacientesMenores(int cantidad);
+    public void setPacientesMasculinos(int cantidad);
+    public void setPacientesFemeninos(int cantidad);
+}
