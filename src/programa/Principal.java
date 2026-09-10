cc7815c9ac817b1ea28d9bcc22e4cfa865664ac8
BurgosManuel <trapecioinv@gmail.com>
2026-09-10 08:29:38 -0300

    Git init

---
+++ src/programa/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programa;
+
+/**
+ *
+ * @author mburgos
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("PRIMER PARCIAL - PARADIGMAS 2K3 2026");
+        System.out.println("Legajo: 64522");
+        System.out.println("Alumno: Burgos Sergio Manuel");
+    }
+    
+}
